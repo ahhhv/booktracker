36aaddbe4d43be801c2f4becc1bea185c5e93498
Alex Hernández <ahernandezv85@gmail.com>
2025-01-13 12:24:49 +0100

    Initial Commit

---
+++ booktracker/booktrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  booktrackerApp.swift
+//  booktracker
+//
+//  Created by Alex Hernández on 13/1/25.
+//
+
+import SwiftUI
+
+@main
+struct booktrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
